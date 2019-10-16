e01b92e5b2907ea174b99d3eb7b9f064e093eb06
Twan Goosen <twan@clarin.eu>
2019-10-16 11:40:07 +0200

    script to make source tarball

---
+++ make-source-tarball.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+VERSION="$1"
+if [ "${VERSION}" ]; then
+	OUT_FILE="${VERSION}.tar.gz"
+	if [ -e "${OUT_FILE}" ]; then
+		echo "Removing existing ${OUT_FILE}" >&2
+		rm ${OUT_FILE}
+	fi
+	tar zcvf "${VERSION}.tar.gz"  --exclude node_modules --exclude target --exclude .git --exclude "${OUT_FILE}" . >&2
+	echo "Done" >&2
+	echo "${OUT_FILE}"
+else
+	echo "Specify version number"
+	exit 1
+fi
+
