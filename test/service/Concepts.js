198b7f6bba2ec5aaccc6d9014a8a9200ef934fa1
Twan Goosen <twan@clarin.eu>
2023-11-10 13:51:07 +0200

    Test runner for JSON-LD parsing for concepts

--- test/service/Concepts.js
+++ test/service/Concepts.js
@@ -1,35 +1,124 @@
 'use strict';
-var assert = require('chai').assert;
-var jsonld = require('jsonld');
+const jsonld = require('jsonld');
 
-var data = [
-  {
-    "@id": "http://hdl.handle.net/11459/CCR_C-4442_0c97f01b-fb0e-7698-22e9-6751abf19d8c",
-    "@type": "http://www.w3.org/2004/02/skos/core#Concept",
-    "http://www.w3.org/2004/02/skos/core#definition": "Indication of an achievement test as research design for the elicitation within a research study. (source: NaLiDa)",
-    "http://www.w3.org/2004/02/skos/core#notation": "achievementTest",
-    "http://www.w3.org/2004/02/skos/core#prefLabel": [
-      "achievement test",
-      { "@language": "en", "@value": "achievement test" }
-    ]
-  },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-6576_00e92a72-bc75-2f4c-14ac-3e7e7e81d0d6", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "A test assessing an actor's proficiency in a certain language. (source: CLARIN-D)", "http://www.w3.org/2004/02/skos/core#notation": "languageTest", "http://www.w3.org/2004/02/skos/core#prefLabel": ["language test", { "@language": "en", "@value": "language test" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-4588_d239ca3a-da99-9462-d39e-61c47a058a7d", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Indication of the type of instrument used for the elicitation of test data as a normalized test. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "normalizedTest", "http://www.w3.org/2004/02/skos/core#prefLabel": ["normalized test", { "@language": "en", "@value": "normalized test" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-4443_609384f0-6920-a202-2b59-b88e0d4eede0", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Indication of a personality test as research design for the elicitation within a research study. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "personalityTest", "http://www.w3.org/2004/02/skos/core#prefLabel": ["personality test", { "@language": "en", "@value": "personality test" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-4544_a1d17d6f-9aff-cb4d-3f9a-3656ea02d05b", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Indication of the design type used for the elicitation of experimental data as a pretest-posttest design. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "pretestPosttestDesign", "http://www.w3.org/2004/02/skos/core#prefLabel": ["pretest-posttest design", { "@language": "en", "@value": "pretest-posttest design" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-4587_0d14e357-7f0e-31f1-c80e-1b24cd76b5be", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Indication of the type of instrument used for the elicitation of test data as a standardized test. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "standardizedTest", "http://www.w3.org/2004/02/skos/core#prefLabel": ["standardized test", { "@language": "en", "@value": "standardized test" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-4367_c9696d87-7039-55d4-f661-2a96fac95f38", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Indication of the class of a resource as test data. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "testData", "http://www.w3.org/2004/02/skos/core#prefLabel": ["test data", { "@language": "en", "@value": "test data" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-3870_9777e4e9-4826-090a-a966-3cb7e726b92f", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Indication of the type of elicitation used for experimental test data within a research study. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "testType", "http://www.w3.org/2004/02/skos/core#prefLabel": ["test type", { "@language": "en", "@value": "test type" }] },
-  { "@id": "http://hdl.handle.net/11459/CCR_C-4430_4e43a338-df90-a14e-151e-95b685541295", "@type": "http://www.w3.org/2004/02/skos/core#Concept", "http://www.w3.org/2004/02/skos/core#definition": "Pertaining to the testing research design of a study. (source: NaLiDa)", "http://www.w3.org/2004/02/skos/core#notation": "testing", "http://www.w3.org/2004/02/skos/core#prefLabel": ["testing", { "@language": "en", "@value": "testing" }] }
-];
+const testData = {
+  "uri": "",
+  "results": [{
+    "uri": "http://hdl.handle.net/11459/CCR_C-4442_0c97f01b-fb0e-7698-22e9-6751abf19d8c",
+    "type": "skos:Concept",
+    "notation": "achievementTest",
+    "prefLabel": ["achievement test", {
+      "@language": "en",
+      "@value": "achievement test"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-6576_00e92a72-bc75-2f4c-14ac-3e7e7e81d0d6",
+    "type": "skos:Concept",
+    "notation": "languageTest",
+    "prefLabel": ["language test", {
+      "@language": "en",
+      "@value": "language test"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-4588_d239ca3a-da99-9462-d39e-61c47a058a7d",
+    "type": "skos:Concept",
+    "notation": "normalizedTest",
+    "prefLabel": ["normalized test", {
+      "@language": "en",
+      "@value": "normalized test"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-4443_609384f0-6920-a202-2b59-b88e0d4eede0",
+    "type": "skos:Concept",
+    "notation": "personalityTest",
+    "prefLabel": ["personality test", {
+      "@language": "en",
+      "@value": "personality test"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-4544_a1d17d6f-9aff-cb4d-3f9a-3656ea02d05b",
+    "type": "skos:Concept",
+    "notation": "pretestPosttestDesign",
+    "prefLabel": ["pretest-posttest design", {
+      "@language": "en",
+      "@value": "pretest-posttest design"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-4587_0d14e357-7f0e-31f1-c80e-1b24cd76b5be",
+    "type": "skos:Concept",
+    "notation": "standardizedTest",
+    "prefLabel": ["standardized test", {
+      "@language": "en",
+      "@value": "standardized test"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-4367_c9696d87-7039-55d4-f661-2a96fac95f38",
+    "type": "skos:Concept",
+    "notation": "testData",
+    "prefLabel": ["test data", {
+      "@language": "en",
+      "@value": "test data"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-3870_9777e4e9-4826-090a-a966-3cb7e726b92f",
+    "type": "skos:Concept",
+    "notation": "testType",
+    "prefLabel": ["test type", {
+      "@language": "en",
+      "@value": "test type"
+    }]
+  }, {
+    "uri": "http://hdl.handle.net/11459/CCR_C-4430_4e43a338-df90-a14e-151e-95b685541295",
+    "type": "skos:Concept",
+    "notation": "testing",
+    "prefLabel": ["testing", {
+      "@language": "en",
+      "@value": "testing"
+    }]
+  }],
+  "@context": {
+    "uri": "@id",
+    "type": "@type",
+    "rdfs": "http://www.w3.org/2000/01/rdf-schema#",
+    "skos": "http://www.w3.org/2004/02/skos/core#",
+    "onki": "http://schema.onki.fi/onki#",
+    "isothes": "http://purl.org/iso25964/skos-thes#",
+    "prefLabel": "skos:prefLabel",
+    "altLabel": "skos:altLabel",
+    "hiddenLabel": "skos:hiddenLabel",
+    "definition": "skos:definition",
+    "notation": "skos:notation",
+    "results": {
+      "@id": "onki:results",
+      "@container": "@list"
+    }
+  }
+}
 
-describe('Concepts', function () {
-  var processor = jsonld.JsonLdProcessor;
+const context = {
+  "uri": "@id",
+  "type": "@type",
+  "skos": "http://www.w3.org/2004/02/skos/core#",
+  "isothes": "http://purl.org/iso25964/skos-thes#",
+  "onki": "http://schema.onki.fi/onki#",
+  "prefLabel": "skos:prefLabel",
+  "altLabel": "skos:altLabel",
+  "hiddenLabel": "skos:hiddenLabel",
+  "definition": "skos:definition",
+  "result": { "@id": "onki:results", "@container": "@list" }
+};
 
-  describe('parse', function () {
-    var json = JSON.parse('{"@context":{"skos":"http:\/\/www.w3.org\/2004\/02\/skos\/core#","isothes":"http:\/\/purl.org\/iso25964\/skos-thes#","onki":"http:\/\/schema.onki.fi\/onki#","uri":"@id","type":"@type","results":{"@id":"onki:results","@container":"@list"},"prefLabel":"skos:prefLabel","altLabel":"skos:altLabel","hiddenLabel":"skos:hiddenLabel","definition":"skos:definition"},"uri":"","results":[{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4442_0c97f01b-fb0e-7698-22e9-6751abf19d8c","type":["skos:Concept"],"prefLabel":"achievement test","lang":"en","notation":"achievementTest","vocab":"ccr","exvocab":"???","definition":["Indication of an achievement test as research design for the elicitation within a research study. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-6576_00e92a72-bc75-2f4c-14ac-3e7e7e81d0d6","type":["skos:Concept"],"prefLabel":"language test","lang":"en","notation":"languageTest","vocab":"ccr","exvocab":"???","definition":["A test assessing an actors proficiency in a certain language. (source: CLARIN-D)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4588_d239ca3a-da99-9462-d39e-61c47a058a7d","type":["skos:Concept"],"prefLabel":"normalized test","lang":"en","notation":"normalizedTest","vocab":"ccr","exvocab":"???","definition":["Indication of the type of instrument used for the elicitation of test data as a normalized test. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4443_609384f0-6920-a202-2b59-b88e0d4eede0","type":["skos:Concept"],"prefLabel":"personality test","lang":"en","notation":"personalityTest","vocab":"ccr","exvocab":"???","definition":["Indication of a personality test as research design for the elicitation within a research study. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4544_a1d17d6f-9aff-cb4d-3f9a-3656ea02d05b","type":["skos:Concept"],"prefLabel":"pretest-posttest design","lang":"en","notation":"pretestPosttestDesign","vocab":"ccr","exvocab":"???","definition":["Indication of the design type used for the elicitation of experimental data as a pretest-posttest design. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4587_0d14e357-7f0e-31f1-c80e-1b24cd76b5be","type":["skos:Concept"],"prefLabel":"standardized test","lang":"en","notation":"standardizedTest","vocab":"ccr","exvocab":"???","definition":["Indication of the type of instrument used for the elicitation of test data as a standardized test. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4367_c9696d87-7039-55d4-f661-2a96fac95f38","type":["skos:Concept"],"prefLabel":"test data","lang":"en","notation":"testData","vocab":"ccr","exvocab":"???","definition":["Indication of the class of a resource as test data. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-3870_9777e4e9-4826-090a-a966-3cb7e726b92f","type":["skos:Concept"],"prefLabel":"test type","lang":"en","notation":"testType","vocab":"ccr","exvocab":"???","definition":["Indication of the type of elicitation used for experimental test data within a research study. (source: NaLiDa)"]},{"uri":"http:\/\/hdl.handle.net\/11459\/CCR_C-4430_4e43a338-df90-a14e-151e-95b685541295","type":["skos:Concept"],"prefLabel":"testing","lang":"en","notation":"testing","vocab":"ccr","exvocab":"???","definition":["Pertaining to the testing research design of a study. (source: NaLiDa)"]}]}');
+async function test() {
+  const processor = jsonld.JsonLdProcessor;
+  const printJson = result => console.log(JSON.stringify(result));
 
-    processor.compact(json);
-  });
+  const compacted = await processor.compact(testData, context);
+  console.log('Compacted:\n');
+  printJson(compacted);
 
-});
+  const expanded = await processor.expand(testData);
+  console.log('\nExpanded:\n');
+  printJson(expanded);
+}
+
+test().then(() => console.log('\nDone'));
