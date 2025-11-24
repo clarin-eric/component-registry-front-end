80ce89baa2e697000f6e48698035d2073c6806b2
Twan Goosen <twan@clarin.eu>
2025-11-24 17:47:52 +0200

    disabled travis job

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ esac
 shift # past argument or value
 done
 
-[ "${INSTALL}" = 1 ] && bash "${SCRIPT_DIR}/npm-docker.sh" "install"
-[ "${BUILD}" = 1 ] && bash "${SCRIPT_DIR}/npm-docker.sh" "run" "build"
+[ "${INSTALL}" = 1 ] && (cd "$SCRIPT_DIR" && docker-compose run --rm ui npm install)
+[ "${BUILD}" = 1 ] && (cd "$SCRIPT_DIR" && docker-compose run --rm ui npm run build)
 
 
